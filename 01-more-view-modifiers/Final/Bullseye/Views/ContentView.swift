0a5acbf516622ba84fa0a52dc5f578c16597a8d2
Franklin Byaruhanga <byaruhaf@protonmail.com>
2026-08-29 17:29:22 +0300

    Update for Module 1

---
+++ 01-more-view-modifiers/Final/Bullseye/Views/ContentView.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+
+struct ContentView: View {
+  @State private var alertIsVisible = false
+  @State private var sliderValue = 50.0
+  @State private var game = Game()
+
+  var body: some View {
+    VStack {
+      Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2.0)
+        .padding(.horizontal, 30)
+      Text(String(game.target))
+        .kerning(-1.0)
+        .font(.largeTitle)
+        .fontWeight(.black)
+      HStack {
+        Text("1")
+          .bold()
+        Slider(value: $sliderValue, in: 1.0...100.0)
+        Text("100")
+          .bold()
+      }
+      .padding()
+      Button("Hit me".uppercased()) {
+        alertIsVisible = true
+      }
+      .padding(20.0)
+      .background(Color.blue)
+      .foregroundColor(.white)
+      .cornerRadius(21.0)
+      .bold()
+      .font(.title3)
+      .alert(
+        "Hello there!",
+        isPresented: $alertIsVisible,
+        actions: {
+          Button("Awesome") {
+            print("Alert closed")
+          }
+        },
+        message: {
+          let roundedValue = Int(sliderValue.rounded())
+          Text("""
+            The slider's value is \(roundedValue).
+            You scored \(game.points(sliderValue: roundedValue)) points this round.
+          """)
+        }
+      )
+    }
+  }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 06-challenge-extract-views/Starter/Bullseye/Views/TextViews.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct InstructionText: View {
+  var text: String
+
+  var body: some View {
+    Text(text.uppercased())
+      .bold()
+      .multilineTextAlignment(.center)
+      .lineSpacing(4.0)
+      .font(.footnote)
+      .kerning(2.0)
+      .foregroundColor(Color("TextColor"))
+  }
+}
+
+struct BigNumberText: View {
+  var text: String
+
+  var body: some View {
+    Text(text)
+      .kerning(-1.0)
+      .font(.largeTitle)
+      .fontWeight(.black)
+      .foregroundColor(Color("TextColor"))
+  }
+}
+
+#Preview {
+    VStack {
+      InstructionText(text: "Instructions")
+      BigNumberText(text: "999")
+}
+}
